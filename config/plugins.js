e40758ffebd5fcb57e2685be207a1fe21314edf1
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-09-16 12:11:01 -0400

    changes

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,6 @@
 module.exports = ({ env }) => ({
     upload: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-    },
+      provider: "cloudinary"
+    }
   });
   
